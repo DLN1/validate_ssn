7f44eee308a28871ce8eb2cd64c6281497578293
Jacob Carlberg <Jacobcarlberg@outlook.com>
2023-09-15 10:57:10 +0200

    replace gsub with split method

--- lib/validate_ssn.rb
+++ lib/validate_ssn.rb
@@ -1,44 +1,41 @@
 
 class ValidateSSN
     def validate(ssn)
-      ssn = ssn.gsub(/[^0-9]/, '')
-  
-      return false unless ssn.length == 10
-  
-    
-      birthdate = ssn[0, 6]
-      last_four = ssn[6, 4]
-  
-     
+      ssnLength = ssn.length
+      return false unless ssnLength == 11 || ssnLength == 13
+
+      birthdate = (ssnLength == 11) ? ssn.split("-")[0] : ssn.split("-")[0][2, 6]
+      last_four = ssn.split("-")[1]
+      puts birthdate
       return calculate_checksum(birthdate, last_four);
     end
-  
+
     def calculate_checksum(birthdate, last_four)
         digits = birthdate + last_four[0, 3]
         checksum = 0
-      
+
 
         digits.each_char.with_index do |char, index|
           digit = char.to_i
-      
+
           if index.even?
             digit *= 2
-      
+
 
             digit -= 9 if digit > 9
           end
-          
+
 
           checksum += digit
         end
-      
+
         last_checksum = checksum*9%10
         last_control_digit = last_four[3,4]
 
         last_checksum.to_i == last_control_digit.to_i
       end
   end
-  
 
 
-ValidateSSN.new.validate("970409-3914")
+
+puts ValidateSSN.new.validate("970409-3914") ? "The SSN is valid" : "The SSN is invalid"
--- spec/validate_ssn_spec.rb
+++ spec/validate_ssn_spec.rb
@@ -2,13 +2,13 @@ require './lib/validate_ssn';
 
 describe ValidateSSN do
     describe ".validate" do
-        
+
     it "SSN should be valid" do
         validateSSN = ValidateSSN.new
-        expect(validateSSN.validate("970409-3914")).to eq(true)
+        expect(validateSSN.validate("19970409-3914")).to eq(true)
     end
     it "SSN should be invalid" do
-        validateSSN = ValidateSSN.new 
+        validateSSN = ValidateSSN.new
         expect(validateSSN.validate("970409-39144")).to eq(false)
     end
 end
